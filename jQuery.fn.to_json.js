dd5421aa38215020b6784f429a731f1de1edd98c
Ben <ben@zfben.com>
2012-07-02 09:43:07 +0800

    v0.1

---
+++ jQuery.fn.to_json.js
@@ -0,0 +1,67 @@
+(function(){
+  var $ = this.jQuery;
+  
+  var get_data_node = function(data, keys){
+    var node = data;
+    for(i in keys){
+      if(typeof node[keys[i]] === 'undefined'){
+        node[keys[i]] = {}
+      }
+      node = node[keys[i]];
+    }
+    return node;
+  }
+  
+  $.fn.to_json = function(){
+    var data = {};
+    this.find(':input').each(function(){
+      var self = $(this);
+      var name = self.attr('name');
+      var type = self.attr('type');
+      if(typeof type === 'undefined'){
+        type = self[0].tagName;
+      }
+      var value = self.val();
+      
+      if(typeof name === 'undefined' || name === null || name === ''){
+        return true;
+      }
+      
+      switch(type){
+        case 'checkbox':
+        case 'radio':
+          if(!self.is(':checked')){
+            return true;
+          }
+          break;
+        case 'INPUT':
+          if(value === '' && typeof self.attr('list') !== 'undefined'){
+            value = $('#' + self.attr('list') + ' :selected').val();
+          }
+          break;
+      }
+      
+      var keys = name.split('.');
+      if(keys.length > 1){
+        var key = keys.pop();
+        var node = get_data_node(data, keys);
+      }else{
+        var key = keys[0];
+        var node = data;
+      }
+      
+      if(/\[\]$/.test(key)){
+        key = key.substring(0, key.length - 2);
+        if(typeof node[key] === 'undefined'){
+          node[key] = [value];
+        }else{
+          node[key].push(value);
+        }
+      }else{
+        node[key] = value;
+      }
+
+    });
+    return data;
+  }
+}).call(this);
---
+++ test.js
@@ -0,0 +1,66 @@
+$(function(){
+  test('jQuery.fn.to_json', function() {
+    ok(jQuery.fn.to_json, 'function is exists');
+  });
+  
+  test('Input Types', function(){
+    deepEqual($('#InputTypes_all_types').to_json(), {
+      no_type: 'no_type',
+      button: 'button',
+      checkbox: 'checkbox',
+      date: '2012-12-21',
+      datetime: 'datetime',
+      'datetime-local': 'datetime-local',
+      email: 'email',
+      file: '',
+      hidden: 'hidden',
+      image: 'image',
+      month: 'month',
+      number: '0',
+      password: 'password',
+      radio: 'radio',
+      range: '1',
+      reset: 'reset',
+      submit: 'submit',
+      text: 'text',
+      time: 'time',
+      url: 'url',
+      week: 'week',
+      select: 'select',
+      textarea: 'textarea',
+      datalist: 'datalist'
+    }, 'All Types');
+    
+    deepEqual($('#InputTypes_checkbox').to_json(), {
+      checked: 'on'
+    }, 'Checkbox');
+    
+    deepEqual($('#InputTypes_radio').to_json(), {
+      radio_: 'checked'
+    })
+  });
+  
+  test('Data Format', function(){
+    deepEqual($('#DataFormat_0').to_json(), {
+      a: 'a',
+      b: 'b'
+    }, '{a:"a", b:"b"}');
+    
+    deepEqual($('#DataFormat_1').to_json(), {
+      a: {
+        b: 'a.b'
+      }
+    }, 'a:{b:"a.b"}');
+    
+    deepEqual($('#DataFormat_2').to_json(), {
+      a: ['1', '2']
+    }, 'a:["1", "2"]');
+    
+    deepEqual($('#DataFormat_3').to_json(), {
+      a: {
+        b: ['1', '2']
+      },
+      c: 'c'
+    }, 'a:{b:["1", "2"]}, c: "c"');
+  });
+});
